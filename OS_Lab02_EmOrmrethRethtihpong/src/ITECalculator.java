5b00226922afbb00d8a5efdfd1e37761c9bc1056
RethtihpongEM <rithtipongem@gmail.com>
2023-01-21 20:33:37 +0700

    Finish OS

--- OS_Lab02_EmOrmrethRethtihpong/src/ITECalculator.java
+++ OS_Lab02_EmOrmrethRethtihpong/src/ITECalculator.java
@@ -2,122 +2,190 @@ import java.util.Scanner;
 public class ITECalculator {
 
   void createMenu() {
-    System.out.println("a. Arithmetic Operators");
-    System.out.println("b. Trigonometry Operators");
-    System.out.println("c. Bitwise Operators");
+    System.out.println("1. Arithmetic Operators");
+    System.out.println("2. Trigonometry Operators");
+    System.out.println("3. Bitwise Operators");
+    System.out.println("4. Storage Converter");
   }
 
   void arithmeticMenu() {
-    System.out.println("a. Sum operator");
-    System.out.println("b. Minus operator");
-    System.out.println("c. Multiply operator");
-    System.out.println("d. Divide operator");
-    System.out.println("e. Modulo operator");
+    System.out.println("1. Sum operator");
+    System.out.println("2. Minus operator");
+    System.out.println("3. Multiply operator");
+    System.out.println("4. Divide operator");
+    System.out.println("5. Modulo operator");
   }
 
   void trigonometryMenu() {
-    System.out.println("a. Sin in Radian");
-    System.out.println("b. Sin in Degree");
-    System.out.println("c. Cos in Radian");
-    System.out.println("d. Cos in Degree");
-    System.out.println("e. Tan in Radian");
-    System.out.println("f. Tan in Degree");
+    System.out.println("1. Sin in Radian");
+    System.out.println("2. Sin in Degree");
+    System.out.println("3. Cos in Radian");
+    System.out.println("4. Cos in Degree");
+    System.out.println("5. Tan in Radian");
+    System.out.println("6. Tan in Degree");
   }
 
   void bitwiseMenu(){
-    System.out.println("a. OR Operator");
-    System.out.println("b. AND Operator");
-    System.out.println("c. XOR Operator");
-    System.out.println("d. Bitwise inversion Operator");
-    System.out.println("e. Left Shift");
-    System.out.println("f. Right Shift");
+    System.out.println("1. OR Operator");
+    System.out.println("2. AND Operator");
+    System.out.println("3. XOR Operator");
+    System.out.println("4. Bitwise inversion Operator");
+    System.out.println("5. Left Shift");
+    System.out.println("6. Right Shift");
+  }
+
+  void storageConverterMenu(){
+    System.out.println("1. Byte");
+    System.out.println("2. Kilobyte");
+    System.out.println("3. Megabyte");
+    System.out.println("4. Gigabyte");
+    System.out.println("5. Terabyte");
+    System.out.println("6. Petabyte");
   }
 
   public static void main(String[] args) {
 
     int a, b;
     float c;
+    long d;
     ITECalculator obj = new ITECalculator();
     obj.createMenu();
 
     Scanner input = new Scanner(System.in);
     System.out.print("Please select option: ");
 
-    String userInput;
-    userInput = input.nextLine();
+    int userInput;
+    userInput = input.nextInt();
 
-    if (userInput.equals("a")) {
+    if (userInput == 1) {
       obj.arithmeticMenu();
       System.out.print("Please select option: ");
-      userInput = input.nextLine();
-      if (userInput.equals("a") || userInput.equals("b") || userInput.equals("c") || userInput.equals("d") || userInput.equals("e")) {
+      userInput = input.nextInt();
+      if (userInput == 1 || userInput == 2 || userInput == 3 || userInput == 4 || userInput == 5) {
         System.out.print("Enter a: ");
         a = input.nextInt();
         System.out.print("Enter b: ");
         b = input.nextInt();
-        if (userInput.equals("a")) {
+        if (userInput == 1) {
           System.out.println("Result = " + Arithmetic.sum(a, b));
-        } else if (userInput.equals("b")) {
+        } else if (userInput == 2) {
           System.out.println("Result = " + Arithmetic.minus(a, b));
-        } else if (userInput.equals("c")) {
+        } else if (userInput == 3) {
           System.out.println("Result = " + Arithmetic.multiply(a, b));
-        } else if (userInput.equals("d")) {
+        } else if (userInput == 4) {
           System.out.println("Result = " + Arithmetic.divide(a, b));
-        } else if (userInput.equals("e")) {
+        } else if (userInput == 5) {
           System.out.println("Result = " + Arithmetic.modulo(a, b));
         }
       }else {
         System.out.println("Invalid Input");
       }
-    }else if (userInput.equals("b")){
+    }else if (userInput == 2){
       obj.trigonometryMenu();
       System.out.print("Please select option: ");
-      userInput = input.nextLine();
-      if (userInput.equals("a") || userInput.equals("b") || userInput.equals("c") || userInput.equals("d") || userInput.equals("e") || userInput.equals("f")) {
+      userInput = input.nextInt();
+      if (userInput == 1 || userInput == 2 || userInput == 3 || userInput == 4 || userInput == 5 || userInput == 6) {
         System.out.print("Input: ");
         c = input.nextFloat();
-        if (userInput.equals("a")) {
+        if (userInput == 1) {
           System.out.println("Result = " + Trigonometry.sin(c));
-        } else if (userInput.equals("b")){
+        } else if (userInput == 2){
           System.out.println("Result = " + Trigonometry.sind((int)c));
-        } else if (userInput.equals("c")) {
+        } else if (userInput == 3) {
           System.out.println("Result = " + Trigonometry.cos(c));
-        } else if (userInput.equals("d")) {
+        } else if (userInput == 4) {
           System.out.println("Result = " + Trigonometry.cosd((int)c));
-        } else if (userInput.equals("e")) {
+        } else if (userInput == 5) {
           System.out.println("Result = " + Trigonometry.tan(c));
-        } else if (userInput.equals("f")) {
+        } else if (userInput == 6) {
           System.out.println("Result = " + Trigonometry.tand((int)c));
         }
       }else {
         System.out.println("Invalid Input");
       }
-    }else if (userInput.equals("c")){
+    }else if (userInput == 3){
       obj.bitwiseMenu();
       Bitwise obj1 = new Bitwise();
       System.out.print("Please select option: ");
-      userInput = input.nextLine();
-      if (userInput.equals("a") || userInput.equals("b") || userInput.equals("c")) {
+      userInput = input.nextInt();
+      if (userInput == 1 || userInput == 2 || userInput == 3) {
         System.out.print("Input a: ");
         a = input.nextInt();
         System.out.print("Input b: ");
         b = input.nextInt();
-        if (userInput.equals("a")) {
+        if (userInput == 1) {
           obj1.logicalOR(a,b);
-        } else if (userInput.equals("b")) {
+        } else if (userInput == 2) {
           obj1.logicalAND(a,b);
-        } else if (userInput.equals("c")) {
+        } else if (userInput == 3) {
           obj1.XOR(a,b);
         }
-      } else if (userInput.equals("d") || userInput.equals("e") || userInput.equals("f")){
+      } else if (userInput == 4 || userInput == 5 || userInput == 6){
         System.out.print("Input: ");
         a = input.nextInt();
-        if (userInput.equals("d")) {
+        if (userInput == 4) {
           obj1.bitwiseInversion(a);
-        } else if (userInput.equals("e")) {
+        } else if (userInput == 5) {
           obj1.leftShift(a);
-        } else if (userInput.equals("f")) {
+        } else if (userInput == 6) {
           obj1.rightShift(a);
+        }
+      }else {
+        System.out.println("Invalid Input");
+      }
+    }else if (userInput == 4){
+      obj.storageConverterMenu();
+      System.out.print("Please select option: ");
+      userInput = input.nextInt();
+      if (userInput == 1 || userInput == 2 || userInput == 3 || userInput == 4 || userInput == 5 || userInput == 6) {
+        if (userInput == 1) {
+          System.out.print("Enter byte: ");
+          d = input.nextLong();
+          System.out.println("Result = " + StorageConverter.byte2KB(d) + " Kilobyte");
+          System.out.println("Result = " + StorageConverter.byte2MB(d) + " Megabyte");
+          System.out.println("Result = " + StorageConverter.byte2GB(d) + " Gigabyte");
+          System.out.println("Result = " + StorageConverter.byte2TB(d) + " Terabyte");
+          System.out.println("Result = " + StorageConverter.byte2PB(d) + " Petabyte");
+        } else if (userInput == 2) {
+          System.out.print("Enter kilobyte: ");
+          d = input.nextLong();
+          System.out.println("Result = " + StorageConverter.kb2Byte(d) + " Byte");
+          System.out.println("Result = " + StorageConverter.kb2MB(d) + " Megabyte");
+          System.out.println("Result = " + StorageConverter.kb2GB(d) + " Gigabyte");
+          System.out.println("Result = " + StorageConverter.kb2TB(d) + " Terabyte");
+          System.out.println("Result = " + StorageConverter.kb2PB(d) + " Petabyte");
+        } else if (userInput == 3) {
+          System.out.print("Enter Megabyte: ");
+          d = input.nextLong();
+          System.out.println("Result = " + StorageConverter.mb2Byte(d) + " Byte");
+          System.out.println("Result = " + StorageConverter.mb2KB(d) + " Kilobyte");
+          System.out.println("Result = " + StorageConverter.mb2GB(d) + " Gigabyte");
+          System.out.println("Result = " + StorageConverter.mb2TB(d) + " Terabyte");
+          System.out.println("Result = " + StorageConverter.mb2PB(d) + " Petabyte");
+        } else if (userInput == 4) {
+          System.out.print("Enter Gigabyte: ");
+          d = input.nextLong();
+          System.out.println("Result = " + StorageConverter.gb2Byte(d) + " Byte");
+          System.out.println("Result = " + StorageConverter.gb2KB(d) + " Kilobyte");
+          System.out.println("Result = " + StorageConverter.gb2MB(d) + " Megabyte");
+          System.out.println("Result = " + StorageConverter.gb2TB(d) + " Terabyte");
+          System.out.println("Result = " + StorageConverter.gb2PB(d) + " Petabyte");
+        } else if (userInput == 5) {
+          System.out.print("Enter Terabyte: ");
+          d = input.nextLong();
+          System.out.println("Result = " + StorageConverter.tb2Byte(d) + " Byte");
+          System.out.println("Result = " + StorageConverter.tb2KB(d) + " Kilobyte");
+          System.out.println("Result = " + StorageConverter.tb2MB(d) + " Megabyte");
+          System.out.println("Result = " + StorageConverter.tb2GB(d) + " Gigabyte");
+          System.out.println("Result = " + StorageConverter.tb2PB(d) + " Petabyte");
+        } else if (userInput == 6){
+          System.out.print("Enter Petabyte: ");
+          d = input.nextLong();
+          System.out.println("Result = " + StorageConverter.pb2Byte(d) + " Byte");
+          System.out.println("Result = " + StorageConverter.pb2KB(d) + " Kilobyte");
+          System.out.println("Result = " + StorageConverter.pb2MB(d) + " Megabyte");
+          System.out.println("Result = " + StorageConverter.pb2GB(d) + " Gigabyte");
+          System.out.println("Result = " + StorageConverter.pb2TB(d) + " Terabyte");
         }
       }else {
         System.out.println("Invalid Input");
--- OS_Lab02_EmOrmrethRethtihpong/src/StorageConverter.java
+++ OS_Lab02_EmOrmrethRethtihpong/src/StorageConverter.java
@@ -1,63 +1,187 @@
+/**
+ * Class to create storage
+ * Byte, Kilobyte, Megabyte, Gigabyte, Terabyte, Petabyte
+ */
 public class StorageConverter {
   public static double byte2KB(long numByte){
+    /*
+     * Convert byte to kilobyte
+     */
     return (double)numByte/1024;
   }
   public static double byte2MB(long numByte){
+    /*
+     * Convert byte to megabyte
+     */
     return byte2KB(numByte)/1024;
   }
   public static double byte2GB(long numByte){
+    /*
+     * Convert byte to gigabyte
+     */
     return byte2MB(numByte)/1024;
   }
   public static double byte2TB(long numByte){
+    /*
+     * Convert byte to terabyte
+     */
     return byte2GB(numByte)/1024;
   }
   public static double byte2PB(long numByte){
+    /*
+     * Convert byte to petabyte
+     */
     return byte2TB(numByte)/1024;
   }
 
   public static double kb2Byte(long kilobyte){
+    /*
+     * Convert kilobyte to byte
+     */
     return kilobyte*1024;
   }
   public static double kb2MB(long kilobyte){
+    /*
+     * Convert kilobyte to megabyte
+     */
     return (double) kilobyte/1024;
   }
   public static double kb2GB(long kilobyte){
+    /*
+     * Convert kilobyte to gigabyte
+     */
     return kb2MB(kilobyte)/1024;
   }
   public static double kb2TB(long kilobyte){
+    /*
+     * Convert kilobyte to terabyte
+     */
     return kb2GB(kilobyte)/1024;
   }
   public static double kb2PB(long kilobyte){
+    /*
+     * Convert kilobyte to petabyte
+     */
     return kb2TB(kilobyte)/1024;
   }
   public static double mb2Byte(long megabyte){
+    /*
+     * Convert megabyte to byte
+     */
     return mb2KB(megabyte)*1024;
   }
   public static double mb2KB(long megabyte){
+    /*
+     * Convert megabyte to kilobyte
+     */
     return megabyte*1024;
   }
   public static double mb2GB(long megabyte){
+    /*
+     * Convert megabyte to gigabyte
+     */
     return (double) megabyte/1024;
   }
   public static double mb2TB(long megabyte){
+    /*
+     * Convert megabyte to terabyte
+     */
     return mb2GB(megabyte)/1024;
   }
   public static double mb2PB(long megabyte){
+    /*
+     * Convert megabyte to petabyte
+     */
     return mb2TB(megabyte)/1024;
   }
   public static double gb2Byte(long gigabyte){
+    /*
+     * Convert gigabyte to byte
+     */
     return gb2KB(gigabyte)*1024;
   }
   public static double gb2KB(long gigabyte){
+    /*
+     * Convert gigabyte to kilobyte
+     */
     return gb2MB(gigabyte)*1024;
   }
   public static double gb2MB(long gigabyte){
+    /*
+     * Convert gigabyte to megabyte
+     */
     return gigabyte*1024;
   }
   public static double gb2TB(long gigabyte){
+    /*
+     * Convert gigabyte to terabyte
+     */
     return (double) gigabyte/1024;
   }
   public static double gb2PB(long gigabyte){
+    /*
+     * Convert gigabyte to petabyte
+     */
     return gb2TB(gigabyte)/1024;
+  }
+  public static double tb2Byte(long terabyte){
+    /*
+     * Convert terabyte to byte
+     */
+    return tb2KB(terabyte)*1024;
+  }
+  public static double tb2KB(long terabyte){
+    /*
+     * Convert terabyte to kilobyte
+     */
+    return tb2MB(terabyte)*1024;
+  }
+  public static double tb2MB(long terabyte){
+    /*
+     * Convert terabyte to megabyte
+     */
+    return tb2GB(terabyte)*1024;
+  }
+  public static double tb2GB(long terabyte){
+    /*
+     * Convert terabyte to gigabyte
+     */
+    return terabyte*1024;
+  }
+  public static double tb2PB(long terabyte){
+    /*
+     * Convert terabyte to petabyte
+     */
+    return (double) terabyte*1024;
+  }
+  public static double pb2Byte(long petabyte){
+    /*
+     * Convert petabyte to byte
+     */
+    return pb2KB(petabyte)*1024;
+  }
+  public static double pb2KB(long petabyte){
+    /*
+     * Convert petabyte to kilobyte
+     */
+    return pb2MB(petabyte)*1024;
+  }
+  public static double pb2MB(long petabyte){
+    /*
+     * Convert petabyte to megabyte
+     */
+    return pb2GB(petabyte)*1024;
+  }
+  public static double pb2GB(long petabyte){
+    /*
+     * Convert petabyte to gigabyte
+     */
+    return pb2TB(petabyte)*1024;
+  }
+  public static double pb2TB(long petabyte){
+    /*
+     * Convert petabyte to terabyte
+     */
+    return petabyte*1024;
   }
 }
