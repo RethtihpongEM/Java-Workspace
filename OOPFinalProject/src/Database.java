f50d15dc5854706d6cec647ee359d4ea029227ee
RethtihpongEM <rithtipongem@gmail.com>
2023-02-22 17:20:25 +0700

    a

--- OOPFinalProject/src/Database.java
+++ OOPFinalProject/src/Database.java
@@ -1,6 +1,8 @@
 import javax.swing.plaf.nimbus.State;
 import java.sql.*;
-public class Database{
+import java.util.ArrayList;
+
+public class Database extends User{
   String url;
   String qry = "";
   Connection con;
@@ -8,39 +10,42 @@ public class Database{
   ResultSet rs;
   PreparedStatement st;
   boolean searchFound = false;
+  User user;
 
   Database(String dbname, String user,String password, int port) throws Exception {
-    url = "jdbc:mysql://localhost:" + port + "/" + dbname;
+    url = "jdbc:mysql://localhost:" + port + "/" + dbname + "?characterEncoding=utf8";
     con = DriverManager.getConnection(url,user,password);
     statement = con.createStatement();
   }
-  void insertToDatabase(int ID, String username, String password){
-
-    try{
-      qry = "insert into users (id,username,password) values(?,?,?)";
-      st = con.prepareStatement(qry);
-      st.setInt(1, ID);
-      st.setString(2, username);
-      st.setString(3, password);
-      st.executeUpdate();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-  }
-  void searchFromDatabase(int ID)  {
+  public ArrayList<User> fetchFromTable(){
+    ArrayList<User> userList = new ArrayList<>();
     try {
-      qry = "select id,username,password from users where id=?";
-      st = con.prepareStatement(qry);
-      st.setInt(1,ID);
-      rs = st.executeQuery();
-      if(rs.next() == false){
-        System.out.println("Sorry user not found");
-      }else{
-        System.out.println(rs.getString("username"));
+      qry = "select * from users";
+      rs = statement.executeQuery(qry);
+      while (rs.next()){
+        user = new User();
+        user.setUserID(rs.getInt("userID"));
+        user.setFirstName(rs.getString("firstName"));
+        user.setLastName(rs.getString("lastName"));
+        user.setAge(rs.getInt("age"));
+        user.setDate(rs.getString("dateOfBirth"));
+        user.setEmail(rs.getString("email"));
+        user.setPassword(rs.getString("password"));
+        user.setPhone(rs.getString("phone"));
+        user.setGender(rs.getString("gender"));
+        user.setType(rs.getString("type"));
+        user.setAddress(rs.getString("address"));
+        user.setUserName(rs.getString("userName"));
+        userList.add(user);
       }
-    } catch (SQLException e) {
+    }catch (SQLException e) {
       throw new RuntimeException(e);
     }
-  }
 
+    return userList;
+  }
+  public void insertIntoUserTable(){
+    user = new User();
+    qry = "insert into users (firstName,lastName,age,dateOfBirth,email,phone,gender,type,address) values(?,?,?,?,?,?,?,?,?)";
+  }
 }
--- OOPFinalProject/src/Main.java
+++ OOPFinalProject/src/Main.java
@@ -1,54 +1,74 @@
+import javax.swing.*;
 import javax.swing.plaf.nimbus.State;
+import javax.swing.table.DefaultTableModel;
 
 import java.sql.*;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-  static void menu(){
-    System.out.println("==== CRUD OPERATION ====");
-    System.out.println("1. Create user");
-    System.out.println("2. Read user");
-    System.out.println("3. Update user");
-    System.out.println("4. Delete user");
-    System.out.print("Enter a choice");
-  }
-  static void readUserMenu(){
-    System.out.println("==== Read user ====");
-    System.out.println("1. Search by ID");
-    System.out.println("2. Display all user");
-    System.out.print("Enter a choice");
+  static void showAllUserPage(Database database){
+    JFrame f = new JFrame("POS SYSTEM");
+
+    ArrayList<User> userArrayList = database.fetchFromTable();
+    DefaultTableModel header = new DefaultTableModel();
+    header.addColumn("ID");
+    header.addColumn("Username");
+    header.addColumn("First name");
+    header.addColumn("Last name");
+    header.addColumn("Age");
+    header.addColumn("Gender");
+    header.addColumn("Date of Birth");
+    header.addColumn("Address");
+    header.addColumn("Email");
+    header.addColumn("Password");
+    header.addColumn("Phone Number");
+
+    JTable userTable = new JTable(header);
+    DefaultTableModel model = (DefaultTableModel) userTable.getModel();
+    for(int i=0;i<userArrayList.size();i++){
+      model.addRow(new Object[]{
+              userArrayList.get(i).getUserID(),
+              userArrayList.get(i).getUserName(),
+              userArrayList.get(i).getFirstName(),
+              userArrayList.get(i).getLastName(),
+              userArrayList.get(i).getAge(),
+              userArrayList.get(i).getGender(),
+              userArrayList.get(i).getDate(),
+              userArrayList.get(i).getAddress(),
+              userArrayList.get(i).getEmail(),
+              userArrayList.get(i).getPassword(),
+              userArrayList.get(i).getPhone()
+      });
+    }
+    JScrollPane sp = new JScrollPane(userTable);
+    f.add(sp);
+    f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    f.setVisible(true);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
   public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);
-    int input;
     Class.forName("com.mysql.jdbc.Driver");
-    Database database = new Database("POSuser", "root", "", 3306);
-    String inputString;
-    User POSuser;
-    while (true){
-      menu();
-      input = scanner.nextInt();
-      switch (input){
-        case 1:
-          POSuser = new User();
-          System.out.println("1. Insert New Data");
-          System.out.print("Enter ID: ");
-          POSuser.setID(scanner.nextInt());
-          System.out.print("Enter Name : ");
-          scanner.nextLine();
-          POSuser.setUsername(scanner.nextLine());
-          System.out.print("Enter Password : ");
-          POSuser.setPassword(scanner.nextLine());
-          database.insertToDatabase(POSuser.getID(), POSuser.getUsername(), POSuser.getPassword());
-          System.out.println("Data Insert Success");
-          break;
-        case 2:
-          POSuser = new User();
-          System.out.print("Input ID: ");
-          POSuser.setID(scanner.nextInt());
-          database.searchFromDatabase(POSuser.getID());
+    Database database = new Database("possystem","root","",3306);
+//    showAllUserPage(database);
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        tableGUI();
       }
-    }
+    });
+  }
+  private static void tableGUI(){
+    UserTable userTable = new UserTable();
+    JPanel root = userTable.getRootPanel();
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setContentPane(root);
+    frame.pack();
+    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setVisible(true);
   }
 }
